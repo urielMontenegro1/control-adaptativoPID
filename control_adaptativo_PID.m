cb725f38db0b8a98f91349314aee98b6e971ef12
Uriel Montenegro <77483327+urielMontenegro1@users.noreply.github.com>
2022-06-02 19:01:35 -0500

    Add files via upload

---
+++ control_adaptativo_PID.m
@@ -0,0 +1,80 @@
+clear all
+close all
+clc
+
+%entrada
+T=3;
+
+%Ganancias proporcional y derivativa
+w1=2;%proporcional
+w2=0;%derivativa
+
+eta1=0.1;
+eta2=0.2;
+
+e_old=0;
+
+t=0.01;
+
+S=2;
+N=S/t;
+
+%posicion inicial
+xi=0.0;
+%velocidad inicial
+vi=0.0;
+
+xi_plot=zeros(1,N);
+xd_plot=zeros(1,N);
+u_plot=zeros(1,N);
+t_plot=t:t:S;
+w_plot=zeros(2,N);
+
+for i=1:N
+    
+    xd=T;
+    
+    e=xd-xi;
+    %parte proporcional
+    x1=e;
+    %parte derivativa
+    x2=(e-e_old)/T;
+    e_old=e;
+    
+    %parte PD sale la velocidad
+    u=w1*x1+w2*x2;
+    
+    vi=vi+u*t;
+    xi=xi+vi*t+0.5*u*t^2;
+    
+    %actualizacion de pesos
+    w1=w1+eta1*u*x1;
+    w2=w2+eta2*u*x2;
+    
+    %se aseguranq ue las ganacias sean positivas
+    if w1<0
+        w1=0;
+    end
+    
+    if w2<0
+        w2=0;
+    end
+   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    xi_plot(i)=xi;
+    xd_plot(i)=xd;
+    u_plot(i)=u;
+    w_plot(:,i)=[w1;w2];
+end
+
+figure
+hold on
+grid on
+plot(t_plot,xd_plot,'r-','Linewidth',2)
+plot(t_plot,xi_plot,'b-','Linewidth',2)
+title('Seguimiento de trayectoria')
+xlabel('t')
+ylabel('x(t)')
+
+
+
+
---
+++ trajectory.m
@@ -0,0 +1,9 @@
+function [r]=trajectory(i*t,T)
+
+e=T-i;
+
+e_sum=e_sum+e*T;
+
+r=e_sum;
+
+end
